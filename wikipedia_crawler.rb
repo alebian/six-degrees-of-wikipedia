4c32382eeadd9be8ae1fd1405242a690194c28ed
alebian <alebezdjian@gmail.com>
2016-04-12 22:32:12 -0300

    Added cache to improve performance

--- wikipedia_crawler.rb
+++ wikipedia_crawler.rb
@@ -1,27 +1,61 @@
 require 'set'
+require 'json'
 require_relative 'wikipedia_scrapper'
 
 class WikipediaCrawler
+  CACHE_FILE = 'wikipedia_cache.json'
+  CACHE_TIME_TO_SAVE = 6000 # backup every 10 minutes
+
   class << self
     def crawl(from_path, to_path)
       queue = []
       queue.push(Node.new(from_path))
 
+      cache = load_cache
+      last_save = Time.now
+
+      answer = []
+
       print "From #{from_path} crawling to #{to_path} "
       while (current = queue.shift) != nil
-        print '.'
-        internal_links = WikipediaScrapper.internal_links(current.path)
-
         return current.previous_plus_current if current.path == to_path
+
+        if cache.key?(current.path)
+          print 'c'
+          internal_links = cache[current.path]
+        else
+          print '.'
+          cache[current.path] = internal_links = WikipediaScrapper.internal_links(current.path)
+          last_save = save_cache(cache, last_save)
+        end
+
         return current.previous_plus_current << to_path if internal_links.include?(to_path)
 
         internal_links.each do |link|
           queue.push(Node.new(link, current.previous_plus_current)) unless current.previous.include?(link)
         end
       end
+    ensure
+      save_cache(cache)
+      return answer
     end
 
     private
+
+    def save_cache(cache, last_save = nil)
+      return last_save if !last_save.nil? && (last_save + CACHE_TIME_TO_SAVE) > Time.now
+      print 'S'
+      File.delete(CACHE_FILE) if File.exist?(CACHE_FILE)
+      file = File.open(CACHE_FILE, 'w+')
+      file.write(JSON.generate(cache))
+      file.close
+      Time.now unless last_save.nil?
+    end
+
+    def load_cache
+      return {} unless File.exist?(CACHE_FILE)
+      JSON.parse(File.read(CACHE_FILE))
+    end
 
     class Node
       attr_reader :path, :previous, :previous_intents
